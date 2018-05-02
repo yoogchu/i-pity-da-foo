2c2f9a584f6bec2888205cce41706b2186d829e8
Quinton <qjohnson@gatech.edu>
2018-05-02 01:39:32 -0400

    Level select

--- Assets/Scripts/MenuScripts/GameStarter.cs
+++ Assets/Scripts/MenuScripts/GameStarter.cs
@@ -8,4 +8,29 @@ public class GameStarter : MonoBehaviour {
 	public void startGame() { 
 		UnityEngine.SceneManagement.SceneManager.LoadScene ("AlphaDemoFinal");
 	}
+
+	public void MainMenuScreen() { 
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("MainMenu");
+	}
+
+	public void LevelOne() { 
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("Level 1");
+	}
+
+	public void LevelTwo() { 
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("Level 2");
+	}
+
+	public void LevelThree() { 
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("Level 3");
+	}
+
+	public void LevelFour() { 
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("Level 4");
+	}
+
+	public void LevelFive() { 
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("Level 5");
+	}
+
 }
---
+++ Assets/Scripts/MenuScripts/MainMenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public GameObject levelSelectMenu;
+
+	public void OpenLevelSelect() {
+		levelSelectMenu.SetActive(true);
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			if (levelSelectMenu.activeInHierarchy) {
+                levelSelectMenu.SetActive(false);
+			} 
+		}
+	}
+
+}
